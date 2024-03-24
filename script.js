2d97974b5f4edefa64d94e22d724eeae798d9328
atif7luqman <atifluqman28@gmail.com>
2024-03-24 15:21:43 +0500

    Add project to github

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', function() {
+  fetch('header.html')
+    .then(response => response.text())
+    .then(html => {
+      document.getElementById('header-placeholder').innerHTML = html;
+    });
+});
